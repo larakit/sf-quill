957f9f56bfc18a4537009cea605477828e56b9c0
Berdnikov Alexey <berdnikov_ay@taximaxim.ru>
2017-03-06 15:25:36 +0500

    fixed

---
+++ init.php
@@ -0,0 +1,7 @@
+<?php
+\Larakit\StaticFiles\Manager::package('larakit/sf-quilljs')
+    ->setSourceDir('public')
+    ->jsPackage('quill.min.js')
+    ->cssPackage('quill.snow.css')
+    ->cssPackage('quill.bubble.css')
+;
